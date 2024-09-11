2ac14abb27a8419caa33559bef17b35c15042da4
JavaChrist <97617119+JavaChrist@users.noreply.github.com>
2024-09-11 11:55:29 +0200

    Rajout de Calendly, dernière modif avant le responsive

--- users.js
+++ users.js
@@ -2,5 +2,7 @@ const users = [
     { email: "client1@example.com", password: "password1" },
     { email: "client2@example.com", password: "password2" },
     { email: "contact@javachrist.fr", password: "Mm2pJava140114@" },
+    { email: "lizzie.grohens14@gmail.com", password: "14092000" },
+
     // Ajoute d'autres utilisateurs ici
 ];
